ccd13f77fbeb2562adfc2615b464afe8900539de
Sandertv <st.ten.veldhuis@gmail.com>
2017-01-10 09:57:15 +0100

    Fix events with wrong constructor.

--- src/Sandertv/BlockSniper/events/BaseEvent.php
+++ src/Sandertv/BlockSniper/events/BaseEvent.php
@@ -3,11 +3,10 @@
 namespace Sandertv\BlockSniper\events;
 
 use pocketmine\event\plugin\PluginEvent;
-use Sandertv\BlockSniper\Loader;
 
 abstract class BaseEvent extends PluginEvent {
 	
-	public function __construct(Loader $owner) {
+	public function __construct($owner) {
 		$this->owner = $owner;
 	}
 	
