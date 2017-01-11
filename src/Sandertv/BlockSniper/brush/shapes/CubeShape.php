3ff983cdabfdcced7bea9f9b6ed009eb0eb07016
Sandertv <st.ten.veldhuis@gmail.com>
2017-01-11 18:09:13 +0100

    I might need some help with this...

--- src/Sandertv/BlockSniper/brush/shapes/CubeShape.php
+++ src/Sandertv/BlockSniper/brush/shapes/CubeShape.php
@@ -13,7 +13,6 @@ class CubeShape extends BaseShape {
 	
 	public function __construct(Loader $main, Level $level, float $radius = null, Vector3 $center = null, array $blocks = []) {
 		parent::__construct($main);
-		$this->main = $main;
 		$this->level = $level;
 		$this->radius = $radius;
 		$this->center = $center;
@@ -25,10 +24,6 @@ class CubeShape extends BaseShape {
 		if(!isset($blocks)) {
 			$this->blocks = ["Air"];
 		}
-	}
-	
-	public function getMain(): Loader {
-		return $this->main;
 	}
 	
 	/**
