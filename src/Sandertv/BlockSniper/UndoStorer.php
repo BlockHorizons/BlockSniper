c72d05eae5ee9c8ded5d52291a1f842f12dc8649
Sandertv <st.ten.veldhuis@gmail.com>
2017-01-13 20:46:03 +0100

    So much messing around.

--- src/Sandertv/BlockSniper/UndoStorer.php
+++ src/Sandertv/BlockSniper/UndoStorer.php
@@ -44,7 +44,7 @@ class UndoStorer {
 		$this->getOwner()->getLogger()->info("Restoring undo save...");
 		foreach($this->undoStore[max(array_keys($this->undoStore))] as $key => $block) {
 			$Id = explode("(", key($block));
-			var_dump(key($key));
+			var_dump($key);
 			var_dump(key($block));
 			$blockId = $Id[0];
 			$x = $block["x"];
