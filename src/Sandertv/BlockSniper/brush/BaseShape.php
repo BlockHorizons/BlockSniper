1af88532448b43fe3bc3394c179f2eff2e6c3532
Sandertv <st.ten.veldhuis@gmail.com>
2017-02-13 20:53:35 +0100

    Added latest type and shape constants.

--- src/Sandertv/BlockSniper/brush/BaseShape.php
+++ src/Sandertv/BlockSniper/brush/BaseShape.php
@@ -8,9 +8,10 @@ abstract class BaseShape {
 	
 	const MAX_WORLD_HEIGHT = 256;
 	const MIN_WORLD_HEIGHT = 0;
+	
 	const SHAPE_CUBE = 0;
-	const SHAPE_SPHERE = 1, SHAPE_BALL = 1;
-	const SHAPE_CYLINDER = 2, SHAPE_STANDING_CYLINDER = 2;
+	const SHAPE_SPHERE = 1;
+	const SHAPE_CYLINDER = 2;
 	const SHAPE_CUBOID = 3;
 	
 	public function __construct(Loader $main) {
