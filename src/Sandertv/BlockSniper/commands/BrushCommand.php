57c983956cee09ac3ae93dbb102a8e91841f3792
Sandertv <st.ten.veldhuis@gmail.com>
2017-02-13 16:45:00 +0100

    Removed debug from Biome brush, added a new event for developers and changed the Brush command a bit.

--- src/Sandertv/BlockSniper/commands/BrushCommand.php
+++ src/Sandertv/BlockSniper/commands/BrushCommand.php
@@ -8,6 +8,7 @@ use pocketmine\Player;
 use pocketmine\utils\TextFormat as TF;
 use Sandertv\BlockSniper\brush\Brush;
 use Sandertv\BlockSniper\Loader;
+use Sandertv\BlockSniper\events\ChangeBrushPropertiesEvent as Change;
 
 class BrushCommand extends BaseCommand {
 	
@@ -44,7 +45,8 @@ class BrushCommand extends BaseCommand {
 				}
 				Brush::setSize($sender, $args[1]);
 				$sender->sendMessage(TF::GREEN . "Size: " . TF::AQUA . $args[1]);
-				return true;
+				$action = Change::ACTION_CHANGE_SIZE;
+				break;
 			
 			case "sh":
 			case "shape":
@@ -59,12 +61,14 @@ class BrushCommand extends BaseCommand {
 						}
 						Brush::setShape($sender, $args[1]);
 						$sender->sendMessage(TF::GREEN . "Shape: " . TF::AQUA . Brush::getShape($sender)->getName());
-						return true;
+						$action = Change::ACTION_CHANGE_SHAPE;
+						break;
 					
 					default:
 						$sender->sendMessage(TF::RED . "[Warning] " . $this->getPlugin()->getTranslation("commands.errors.shape-not-found"));
 						return true;
 				}
+				break;
 			
 			case "ty":
 			case "type":
@@ -87,12 +91,14 @@ class BrushCommand extends BaseCommand {
 						}
 						Brush::setType($sender, $args[1]);
 						$sender->sendMessage(TF::GREEN . "Type: " . TF::AQUA . Brush::getType($sender)->getName());
-						return true;
-					
+						$action = Change::ACTION_CHANGE_TYPE;
+						break;
+						
 					default:
 						$sender->sendMessage(TF::RED . "[Warning] " . $this->getPlugin()->getTranslation("commands.errors.shape-not-found"));
 						return true;
 				}
+				break;
 			
 			case "he":
 			case "height":
@@ -102,7 +108,8 @@ class BrushCommand extends BaseCommand {
 				}
 				Brush::setHeight($sender, $args[1]);
 				$sender->sendMessage(TF::GREEN . "Height: " . TF::AQUA . $args[1]);
-				return true;
+				$action = Change::ACTION_CHANGE_HEIGHT;
+				break;
 			
 			case "bl":
 			case "block":
@@ -115,14 +122,16 @@ class BrushCommand extends BaseCommand {
 					$blockNames[] = $block->getName();
 				}
 				$sender->sendMessage(TF::GREEN . "Blocks: " . TF::AQUA . implode(", ", $blockNames));
-				return true;
+				$action = Change::ACTION_CHANGE_BLOCKS;
+				break;
 			
 			case "ob":
 			case "obsolete":
 			case "replaced":
 				Brush::setObsolete($sender, $args[1]);
 				$sender->sendMessage(TF::GREEN . "Obsolete: " . TF::AQUA . Brush::getObsolete($sender)->getName());
-				return true;
+				$action = Change::ACTION_CHANGE_OBSOLETE;
+				break;
 			
 			case "pe":
 			case "perfect":
@@ -130,7 +139,7 @@ class BrushCommand extends BaseCommand {
 				$sender->sendMessage(TF::GREEN . "Perfect: " . TF::AQUA . $args[1]);
 				return true;
 			
-			case "gr":
+			case "gr": // TODO: Add action for ChangeBrushPropertiesEvent once stable.
 			case "gravity":
 				Brush::setGravity($sender, $args[1]);
 				$sender->sendMessage(TF::GREEN . "Gravity: " . TF::AQUA . $args[1]);
@@ -142,18 +151,22 @@ class BrushCommand extends BaseCommand {
 				Brush::setDecrementing($sender, $args[1]);
 				Brush::$resetSize[$sender->getId()] = Brush::getSize($sender);
 				$sender->sendMessage(TF::GREEN . "Decrement: " . TF::AQUA . $args[1]);
-				return true;
+				$action = Change::ACTION_CHANGE_DECREMENT;
+				break;
 			
 			case "bi":
 			case "biome":
 				$biome = array_slice($args, 1);
 				Brush::setBiome($sender, implode(" ", $biome));
 				$sender->sendMessage(TF::GREEN . "Biome: " . TF::AQUA . Biome::getBiome(Brush::getBiomeIdFromString($sender))->getName());
-				return true;
+				$action = Change::ACTION_CHANGE_BIOME;
+				break;
 			
 			default:
 				$sender->sendMessage(TF::RED . "[Usage] /brush <size|shape|type|blocks|height|obsolete|perfect> <value>");
 				return true;
 		}
+		$this->getPlugin()->getServer()->getPluginManager()->callEvent(new Change($this->getPlugin(), $sender, $action));
+		return true;
 	}
 }
