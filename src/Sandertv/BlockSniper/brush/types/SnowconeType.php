b85cd44586f16fa913862321b49ba8e805dc890e
Sandertv <st.ten.veldhuis@gmail.com>
2017-03-23 20:45:11 +0100

    Fixed snow cone not affecting normal ground.

--- src/Sandertv/BlockSniper/brush/types/SnowconeType.php
+++ src/Sandertv/BlockSniper/brush/types/SnowconeType.php
@@ -29,10 +29,10 @@ class SnowconeType extends BaseType {
 			if($block->getId() !== Block::AIR && ($block->getId() !== Block::SNOW_LAYER || ($block->getDamage() % 7 === 0 && $block->getId() === Block::SNOW_LAYER))) {
 				$topBlock = $block->getSide(Block::SIDE_UP);
 				if($topBlock->getId() === Block::AIR || $topBlock->getId() === Block::SNOW_LAYER) {
-					if($topBlock->getDamage() < 7) {
+					if($topBlock->getDamage() < 7 && $topBlock->getId() === Block::SNOW_LAYER) {
 						$undoBlocks[] = $topBlock;
 						$this->level->setBlockDataAt($topBlock->x, $topBlock->y, $topBlock->z, $topBlock->getDamage() + 1);
-					} else {
+					} elseif($topBlock->getId() !== Block::SNOW_LAYER) {
 						$undoBlocks[] = $block->getSide(Block::SIDE_UP);
 						$this->level->setBlock($block->getSide(Block::SIDE_UP), Block::get(Block::SNOW_LAYER), false, false);
 					}
