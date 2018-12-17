c637786004b59ddc48e2e0fa61e56b6f19dde0c6
JackMD <jackmtaylor.jmt@gmail.com>
2018-12-18 00:52:44 +0500

    Fix artifact_url that previously was messing up the .phar

--- src/BlockHorizons/BlockSniper/tasks/UpdateNotifyTask.php
+++ src/BlockHorizons/BlockSniper/tasks/UpdateNotifyTask.php
@@ -46,6 +46,7 @@ class UpdateNotifyTask extends AsyncTask{
 		if($highestVersion === Loader::VERSION){
 			return;
 		}
+		$artifactUrl = $artifactUrl . "/BlockSniper_" . $highestVersion . ".phar";
 		$loader->getLogger()->info(vsprintf("Version %s has been released for API %s. Download the new release at %s",
 											[$highestVersion, $api, $artifactUrl]));
 	}
